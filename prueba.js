0344267b958601b613d67a5eb2f57ff628cf22bc
Alan Perez <117047151+Alanmrb@users.noreply.github.com>
2022-11-19 15:37:51 -0300

    Add files via upload

--- prueba.js
+++ prueba.js
@@ -2,15 +2,25 @@ const loquehayenlocal = 'listaUsuarios';
 let almacenados = JSON.parse(localStorage.getItem(loquehayenlocal)) || [] ;
 let emailstecnicos = [];
 let tecnicos = [] ; 
+let almacenadosnuevo ; 
 let btn_crear = document.getElementById('btn_crear');
 let nombre = document.getElementById('nombre');
 let apellido = document.getElementById('apellido');
 let email = document.getElementById('email');
 let btnadmin = document.getElementById('btnadmin');
+let usuarios;
+
+//traer con fetch el admin y guardarlo en usuarios para validar al mostrar usuarios
+fetch('admin.json')
+        .then((res)=> res.json())
+        .then(data => {usuarios = data
+        console.log(usuarios)})
+
+
 
 btnadmin.addEventListener('click', () => {
 
-    mostrarusuarios(almacenados);
+    mostrarusuarios(almacenados,tecnicos);
     
 });
 
@@ -28,7 +38,7 @@ btn_crear.addEventListener('click', () => {
 
 //recuperamos datos del local o array vacio y lo guardamos
 function recuperardatos() {
-    let almacenados = JSON.parse(localStorage.getItem(loquehayenlocal)) || [] ;
+    let almacenados = JSON.parse(localStorage.getItem(loquehayenlocal)) ;
     console.log(almacenados);
 };
 
@@ -47,8 +57,9 @@ function agregarusuario() {
 
     } else {
 //si el usuario no existe, pushea el objeto nuevo en tecnicos y el mismo se guarda en local
-        tecnicos.push(new tecnico(getLastId(almacenados),nombre, apellido, email));
-        localStorage.setItem('listaUsuarios', JSON.stringify(tecnicos));
+        
+        almacenados.push(new tecnico(getLastId(almacenados),nombre, apellido, email));
+        localStorage.setItem('listaUsuarios', JSON.stringify(almacenados));
         Toastify({
             text: "Bienvenido " + nombre.value + " proximamente recibira un correo con nuestra lista actualizada",
             className: "info",
@@ -60,6 +71,7 @@ function agregarusuario() {
     }
 };
 
+//funcion que valida si el mail ingresado esta o no dentro de lo recuperado del local
 function validacion(){
     recuperardatos(almacenados);
     let emailstecnicos = tecnicos.map((tecnico) => {return (tecnico.email)});
@@ -75,48 +87,47 @@ function validacion(){
 };
 
 
-//funcion que muestra los usuarios guardados en el local
-//guardar en un json al admin y llamarlo con fetch
-
+//funcion que muestra unicamente al admin los usuarios guardados en el local
+//si no es admin devuelve un error
 function mostrarusuarios(almacenados) {
 
-  
-let nombreIngresado =  prompt('ingrese su nombre de usuario');            
-let passIngresado = prompt('ingrese su pass');
+let recuperadode = usuarios.map((Element) => {return (Element.nombre)});
 
 
-fetch('admin.json')
-        .then((res)=> res.json())
-        .then((data)=> console.log(data))
-        .then((data)=> {
-            const recuperadode = data.map((data)=>data.nombre)
-        })
-        .then((recuperadode)=>{
-        if (recuperadode = nombreIngresado){
-            alert('bienvenido');
-        }    
-})};
+Swal.fire({
+        title: "Ingrese Su Pass",
+        input: "text",
+        showCancelButton: true,
+        confirmButtonText: "OK",
+        cancelButtonText: "Cancelar",
+        inputValidator: passingresado => {
+            // Si el valor es válido, debes regresar undefined. Si no, una cadena
+            if (!passingresado) {
+                return "por favor ingrese su pass";
+            } else {
+                if (passingresado == recuperadode ) {
+                    
 
+                    let mensaje = 'Los mails registrados son:';
+                
+                    almacenados.forEach(tecnico => {
+                        mensaje += '\n' + tecnico.nombre + tecnico.email;
+                     
+                    })
+                    Swal.fire(mensaje);
+            }else{
+                Swal.fire('Pass Invalido')
+            }
+        }
+    }
+})
+}
 
-        
-         
-         /*if(nombreIngresado == nombreadmin ) {
-            let mensaje = 'Los mails registrados son:';
-
-            almacenados.forEach(tecnico => {
-                mensaje += '\n' + tecnico.email;
-             
-            })
-            alert(mensaje); 
-        }})
-    
- } */
-        
-
-        
+       
 
 //Funcion que busca el ultimo id y retorna un nuevo id +1
-function getLastId(almacenados) {
+function getLastId() {
+    recuperardatos(almacenados)
     let maxValue = 0;
     almacenados.map((tecnico) => tecnico.id) .forEach((id) => {
         if (id > maxValue) {
@@ -125,7 +136,4 @@ function getLastId(almacenados) {
       });
     return maxValue + 1;
   }
-
-//cambiar los alert y los prompt
-//validar email valido , insertarlo en el html 
 
